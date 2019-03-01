566de6a9a864aa5134a5ad207045d39c7dd2b244
devrabbit <devrabbit@localhost.localdomain>
2019-03-01 11:18:45 +0530

    first commit

---
+++ WebDrivers/browser.sh
@@ -0,0 +1,2 @@
+cd  /Users/devrabbit/Desktop/18-06-FrameworkGit/Framework/WebDrivers
+java -jar TAF_Launch_WebPage.jar "{\"web_page_name\":\"\",\"web_page_description\":\"\",\"diff_page_id\":0,\"jar_type\":1,\"module_id\":323,\"user_id\":7,\"web_page_url\":\"http://newtours.demoaut.com/\",\"save_web_page_and_elements_url\":\"https://192.168.1.142:8080/TAF_Automation_DR/CreateWebPage\"}"
---
+++ gitcommands.bat
@@ -0,0 +1,15 @@
+source ~/.bash_profile
+cd /Users/devrabbit/Desktop/SmartQE
+git init
+ git add .
+git rm -r --cached target
+git rm -r --cached test-output
+git rm -r --cached gitcommands.bat
+git rm -r --cached gitcommands.sh
+git rm -r --cached APIReports
+git rm -r --cached MobileReports
+git rm -r --cached WebReports
+git commit -m "first commit"
+git remote add origin https://santhosh-annam:github7489.srs@github.com/devrabbititsol/SmartQETest.git
+git push -u origin master
+
